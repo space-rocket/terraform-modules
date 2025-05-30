8e0734ad59c576822963fe1a2f5a01f2d7b1860c
Michael Chavez <mchavez@space-rocket.com>
2025-05-30 00:14:31 -0700

    cidr ip

--- 11-bastion/main.tf
+++ 11-bastion/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = ["${var.my_ip}/32"]
   }
 
   egress {
