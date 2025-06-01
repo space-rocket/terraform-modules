c40a15457748dc189a6fa1d4d4c5459e090c39f0
Michael Chavez <mchavez@space-rocket.com>
2025-06-01 13:30:52 -0700

    rm ssm_parameter prefix

--- 04-ecs-cluster/main.tf
+++ 04-ecs-cluster/main.tf
@@ -40,23 +40,23 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_attachment" {
 
 # (Optionally) attach a policy for reading parameters from SSM under the
 # execution role, if you prefer that design
-resource "aws_iam_policy" "ssm_params_policy" {
-  name   = "${var.env}-${var.project}-ssm-params-policy"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = ["ssm:GetParameters"],
-        Resource = [
-          for prefix in var.ssm_secret_path_prefixes : "${prefix}/*"
-        ]
-      }
-    ]
-  })
-}
+# resource "aws_iam_policy" "ssm_params_policy" {
+#   name   = "${var.env}-${var.project}-ssm-params-policy"
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = ["ssm:GetParameters"],
+#         Resource = [
+#           for prefix in var.ssm_secret_path_prefixes : "${prefix}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "ssm_params_policy_attachment" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = aws_iam_policy.ssm_params_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "ssm_params_policy_attachment" {
+#   role       = aws_iam_role.ecs_execution_role.name
+#   policy_arn = aws_iam_policy.ssm_params_policy.arn
+# }
