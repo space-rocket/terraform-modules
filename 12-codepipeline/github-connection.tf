651d362ca452ab531fd1c71a5995bb84681be1da
Michael Chavez <mchavez@space-rocket.com>
2025-06-02 01:37:16 -0700

    rn app_name to task_name

--- 12-codepipeline/github-connection.tf
+++ 12-codepipeline/github-connection.tf
@@ -1,4 +1,4 @@
 resource "aws_codestarconnections_connection" "github_connection" {
-  name          = "${local.app_name}-${local.env}"
+  name          = "${local.task_name}-${local.env}"
   provider_type = "GitHub"
 }
