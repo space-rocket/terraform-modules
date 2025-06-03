8ad1b2b2da5340e1bfabeade297ec8ede3ac5625
Michael Chavez <mchavez@space-rocket.com>
2025-06-03 00:16:23 -0700

    removed stream name from codebuild

--- 12-codepipeline/codebuild-build.tf
+++ 12-codepipeline/codebuild-build.tf
@@ -32,8 +32,7 @@ resource "aws_codebuild_project" "build" {
 
   logs_config {
     cloudwatch_logs {
-      group_name  = "${local.log_group_name}/codebuild"
-      stream_name = "${local.task_name}-build"
+      group_name  = "${local.log_group_name}/codebuild/build"
     }
 
     s3_logs {
