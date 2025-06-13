14f4bfca7957338659d4bcd7a6deaae3c1d589fe
Michael Chavez <mchavez@space-rocket.com>
2025-06-13 15:47:13 -0700

    added main.tf

--- sagemaker/main.tf
+++ sagemaker/main.tf
@@ -1,1 +1,14 @@
-main.tf
+resource "aws_sagemaker_domain" "this" {
+  domain_name = var.domain_name
+  auth_mode   = "IAM"
+  subnet_ids  = var.subnet_ids
+  vpc_id      = var.vpc_id
+  execution_role_arn = var.execution_role_arn
+  tags = var.tags
+}
+
+resource "aws_sagemaker_user_profile" "this" {
+  domain_id         = aws_sagemaker_domain.this.id
+  user_profile_name = var.user_profile_name
+  tags              = var.tags
+}
