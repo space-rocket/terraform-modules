15f9cd64143bc0ceed448e9090c192bcd10939bb
Michael Chavez <mchavez@space-rocket.com>
2025-07-01 14:31:37 -0700

    added ecs get parameter

--- 04-ecs-cluster/main.tf
+++ 04-ecs-cluster/main.tf
@@ -61,3 +61,28 @@ resource "aws_iam_role_policy_attachment" "ssm_params_policy_attachment" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = aws_iam_policy.ssm_params_policy.arn
 }
+
+resource "aws_iam_policy" "ecs_execution_ssm_access" {
+  name = "${local.name_prefix}-ecs-execution-ssm"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowSSMReadForECSExecution",
+        Effect = "Allow",
+        Action = [
+          "ssm:GetParameters",
+          "ssm:GetParameter",
+          "ssm:GetParametersByPath"
+        ],
+        Resource = format("arn:aws:ssm:%s:%s:parameter%s/*", var.region, var.account_id, var.path_prefix)
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_ssm_access" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_ssm_access.arn
+}
