ea31a703d773cd106f60373c0252d3ef879c2735
Michael Chavez <mchavez@space-rocket.com>
2025-07-01 15:49:10 -0700

    app_name and prefix no

--- 04-ecs-cluster/variables.tf
+++ 04-ecs-cluster/variables.tf
@@ -10,16 +10,10 @@ variable "project" {
 variable "region" {
   type = string
 }
-variable "app_name" {
-  type = string
-}
+
 variable "ssm_secret_path_prefixes" {
   description = "List of SSM parameter path prefixes to allow ECS task execution role to read"
   type        = list(string)
-}
-
-variable "ssm_secret_path_prefix" {
-  type = string
 }
 
 variable "cluster_name_override" {
