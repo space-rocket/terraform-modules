9c390e1e1a663ec8d4899db11c14a3f0412e8f30
Michael Chavez <mchavez@space-rocket.com>
2025-07-16 20:35:47 -0700

    increase timeout

--- 12-codepipeline/codebuild-build.tf
+++ 12-codepipeline/codebuild-build.tf
@@ -1,7 +1,7 @@
 resource "aws_codebuild_project" "build" {
   name          = "${local.task_name}"
   description   = "${local.task_name} Codebuild Build Project"
-  build_timeout = 10
+  build_timeout = 15
   service_role  = aws_iam_role.code_build_role.arn
 
   artifacts {
