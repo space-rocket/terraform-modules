29646987f94841ffa00dfb619f78e80b46d81577
Michael Chavez <mchavez@space-rocket.com>
2025-07-26 16:53:25 -0700

    rvert logging s3 bucket changes

--- 03-alb/logging_s3_bucket.tf
+++ 03-alb/logging_s3_bucket.tf
@@ -10,21 +10,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
   }
 }
 
-# resource "aws_s3_bucket_lifecycle_configuration" "logs" {
-#   count = var.logs_bucket == null ? 0 : 1
-
-#   bucket = aws_s3_bucket.logs[0].id
-
-#   rule {
-#     id      = "delete"
-#     status  = "Enabled"
-
-#     expiration {
-#       days = var.logs_expiration
-#     }
-#   }
-# }
-
 resource "aws_s3_bucket_lifecycle_configuration" "logs" {
   count = var.logs_bucket == null ? 0 : 1
 
@@ -33,10 +18,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "logs" {
   rule {
     id      = "delete"
     status  = "Enabled"
-
-    filter {
-      prefix = "${var.logs_prefix}/AWSLogs/${var.account_id}/"
-    }
 
     expiration {
       days = var.logs_expiration
@@ -66,35 +47,13 @@ resource "aws_s3_bucket" "logs" {
 data "aws_iam_policy_document" "alb_logs_s3" {
   count = var.logs_bucket == null ? 0 : 1
 
-  # statement {
-  #   sid    = "AllowALBLogDeliveryPut"
-  #   effect = "Allow"
-
-  #   principals {
-  #     type        = "Service"
-  #     identifiers = ["logdelivery.elasticloadbalancing.amazonaws.com"]
-  #   }
-
-  #   actions = ["s3:PutObject"]
-  #   resources = [
-  #     "${aws_s3_bucket.logs[0].arn}/${var.logs_prefix}/AWSLogs/${var.account_id}/*"
-  #   ]
-
-  #   condition {
-  #     test     = "StringEquals"
-  #     variable = "s3:x-amz-acl"
-  #     values   = ["bucket-owner-full-control"]
-  #   }
-  # }
-
-  # REPLACE your current "AllowALBLogDeliveryPut" with this minimal block
   statement {
     sid    = "AllowALBLogDeliveryPut"
     effect = "Allow"
 
-    principal {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::127311923021:root"] # us-east-1 ELB account
+    principals {
+      type        = "Service"
+      identifiers = ["logdelivery.elasticloadbalancing.amazonaws.com"]
     }
 
     actions = ["s3:PutObject"]
@@ -103,9 +62,9 @@ data "aws_iam_policy_document" "alb_logs_s3" {
     ]
 
     condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:elasticloadbalancing:${var.aws_region}:${var.account_id}:loadbalancer/*"]
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
     }
   }
 
@@ -120,24 +79,6 @@ data "aws_iam_policy_document" "alb_logs_s3" {
 
     actions   = ["s3:GetBucketAcl"]
     resources = [aws_s3_bucket.logs[0].arn]
-  }
-
-  statement {
-    sid    = "AllowBucketOwnerFullControl"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = [var.account_id]
-    }
-
-    actions   = [
-      "s3:PutObject",
-      "s3:GetObject"
-    ] 
-    resources = [
-      "${aws_s3_bucket.logs[0].arn}/${var.logs_prefix}/AWSLogs/${var.account_id}/*"
-    ]
   }
 }
 
