55d1a578b26e0a092cfae47e4e6b28271448b8ab
Michael Chavez <mchavez@space-rocket.com>
2025-07-26 16:55:20 -0700

    rm unneccessary blocks

--- 03-alb/logging_s3_bucket.tf
+++ 03-alb/logging_s3_bucket.tf
@@ -11,13 +11,16 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "logs" {
-  count = var.logs_bucket == null ? 0 : 1
-
+  count  = var.logs_bucket == null ? 0 : 1
   bucket = aws_s3_bucket.logs[0].id
 
   rule {
-    id      = "delete"
-    status  = "Enabled"
+    id     = "delete"
+    status = "Enabled"
+
+    filter {
+      prefix = "${var.logs_prefix}/AWSLogs/${var.account_id}/"
+    }
 
     expiration {
       days = var.logs_expiration
@@ -52,8 +55,8 @@ data "aws_iam_policy_document" "alb_logs_s3" {
     effect = "Allow"
 
     principals {
-      type        = "Service"
-      identifiers = ["logdelivery.elasticloadbalancing.amazonaws.com"]
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::127311923021:root"] # us-east-1 ELB account
     }
 
     actions = ["s3:PutObject"]
@@ -61,24 +64,12 @@ data "aws_iam_policy_document" "alb_logs_s3" {
       "${aws_s3_bucket.logs[0].arn}/${var.logs_prefix}/AWSLogs/${var.account_id}/*"
     ]
 
+    # optional hardening, safe to keep
     condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:elasticloadbalancing:${var.aws_region}:${var.account_id}:loadbalancer/*"]
     }
-  }
-
-  statement {
-    sid    = "AllowALBLogDeliveryGetAcl"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["logdelivery.elasticloadbalancing.amazonaws.com"]
-    }
-
-    actions   = ["s3:GetBucketAcl"]
-    resources = [aws_s3_bucket.logs[0].arn]
   }
 }
 
