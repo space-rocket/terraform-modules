1792924ff2d86816577e060c931fd77ef54d9783
Michael Chavez <mchavez@space-rocket.com>
2025-09-17 22:28:27 -0700

    codepipeline-dry

--- 12-codepipeline-dry/local-values.tf
+++ 12-codepipeline-dry/local-values.tf
@@ -4,21 +4,13 @@ locals {
   project                    = var.project
   environment                = var.env
   env                        = var.env
-  app_name                   = var.app_name
-  task_name                  = var.task_name
   log_group_name             = var.log_group_name
   region                     = var.region
-  # name                       = "${var.project}-${var.env}-${var.app_name}"
   git_repo                   = var.git_repo
   git_branch                 = var.git_branch
   ecs_cluster_name           = var.ecs_cluster_name
-  ecs_service_name           = var.ecs_service_name
-  fargate_cpu                = var.fargate_cpu
-  fargate_memory             = var.fargate_memory
-  fargate_ecs_task_role      = var.fargate_ecs_task_role
-  fargate_ecs_execution_role = var.fargate_ecs_execution_role
   image_repo                 = var.image_repo
-  port                       = var.port
+  
   ssm_secret_path_prefix     = var.ssm_secret_path_prefix
   app_secrets                = var.app_secrets
   common_tags = {
